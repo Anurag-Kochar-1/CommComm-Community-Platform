6ff4982da511494ad057453a78641ba25cced816
Anurag <anuragmarketing101@gmail.com>
2023-01-02 00:31:07 +0530

    auth-modal-added-base

--- components/globalComponents/Header/components/SearchBar/SearchBar.tsx
+++ components/globalComponents/Header/components/SearchBar/SearchBar.tsx
@@ -6,18 +6,12 @@ const SearchBar = () => {
         <div className='hidden md:inline-flex w-[50%] h-[5vh] bg-black justify-center items-center border-2 border-black rounded-sm'>
 
             <div className='w-full h-full bg-white rounded-sm flex justify-between items-center px-4'>
-                <BiSearch className='text-xl text-black mr-1'/>
+                <BiSearch className='text-lg lg:text-xl text-black mr-1'/>
                 <input
                     type="text"
                     placeholder='Search...'
-                    className='w-full h-full bg-white outline-none focus:ring-0 px-2 py-1 placeholder:text-BrutalBlack1 text-BrutalBlack1'
+                    className='w-full h-full bg-white outline-none focus:ring-0 px-2 placeholder:text-BrutalBlack1 text-BrutalBlack1'
                 />
-
-                {/* <button type='button' title='searchIcon' className='relative w-7 h-7 border-black bg-black border rounded-md flex justify-center items-center pr-5'>
-                    <span className='w-7 h-7 rounded-md bg-BrutalPurple1 flex justify-center items-center absolute right-[1px] bottom-[1.5px] border border-black active:bottom-0 active:right-0 hover:bottom-0 hover:right-0'>
-                        <BiSearch className='text-lg text-black' />
-                    </span>
-                </button> */}
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import { useRouter } from 'next/router'
+import SignUpModal from '../components/globalComponents/Modals/AuthenticationModals/SignUpModal/SignUpModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,9 @@ export default function Home() {
   return (
     <main className='w-full lg:w-[70%] h-[80vh] lg:h-[90vh] mt-[10vh] mb-[10vh] lg:mb-0 bg-gray-300 flex flex-col justify-start items-center'>
       <h1 className='text-4xl text-blue-500 font-bold mt-[7vh]'> Home  </h1>
+
+
+      <SignUpModal />
     </main>
   )
 }

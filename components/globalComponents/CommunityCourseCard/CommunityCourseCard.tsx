74957fda009a1ab5146b27dde24e3d92dd2650f8
Anurag <anuragmarketing101@gmail.com>
2023-01-16 16:31:46 +0530

    courseCard-4-details-fixed

--- components/globalComponents/CommunityCourseCard/CommunityCourseCard.tsx
+++ components/globalComponents/CommunityCourseCard/CommunityCourseCard.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { ICourse } from '../../../customTypesAndInterfaces/Course/courseInterfaces'
 import { HiCalendarDays } from "react-icons/hi2"
 import { AiFillYoutube } from 'react-icons/ai'
+import { FiTarget } from 'react-icons/fi'
+import { BsBook } from 'react-icons/bs'
 
 interface IProps {
     communityCourseData: ICourse
@@ -34,8 +36,8 @@ const CommunityCourseCard = ({ communityCourseData }: IProps) => {
 
                     <div className='w-full py-1 flex flex-col justify-start items-center space-y-2 '>
                         {/* ---- Tags ---- */}
-                        <div className='w-full py-1 flex flex-wrap justify-start items-center space-x-2 '>
-                            {/* --- Days --- */}
+                        <div className='w-full py-1 flex lg:flex-col flex-wrap lg:flex-nowrap justify-start items-center lg:items-start space-x-2 lg:space-x-0 lg:space-y-2'>
+                            {/*  Days  */}
                             <div className='flex justify-center items-center space-x-2 bg-white p-1 hover:cursor-pointer'>
                                 <span className='bg-BrutalOrange1 p-1 border border-black rounded-sm'>
                                     <HiCalendarDays />
@@ -50,10 +52,32 @@ const CommunityCourseCard = ({ communityCourseData }: IProps) => {
                                 </span>
                                 <p className='text-black text-sm font-Roboto font-medium'> {communityCourseData?.youtubeCourseChannelName} </p>
                             </div>
+
+                            {/* Goal */}
+                            <div className='hidden  lg:inline-flex justify-start items-center space-x-2 bg-white p-1 hover:cursor-pointer'>
+                                <span className='bg-BrutalBlue1 p-1 border border-black rounded-sm '>
+                                    <FiTarget className='text-black' />
+                                </span>
+                                <div className='flex justify-start items-center space-x-2'>
+                                    <span className='text-black text-sm font-Roboto font-medium'> {"Goal : "} </span>
+                                    <p className='text-black text-sm font-Roboto font-medium'>  {communityCourseData?.courseGoal} </p>
+                                </div>
+                            </div>
+
+                            {/* Prerequisites */}
+                            <div className='hidden  lg:inline-flex justify-start items-center space-x-2 bg-white p-1 hover:cursor-pointer'>
+                                <span className='bg-BrutalYellow1 p-1 border border-black rounded-sm '>
+                                    <BsBook className='text-black' />
+                                </span>
+                                <div className='flex justify-start items-center space-x-2'>
+                                    <span className='text-black text-sm font-Roboto font-medium'> {"Prerequisites : "} </span>
+                                    <p className='text-black text-sm font-Roboto font-medium'>  {communityCourseData?.coursePrerequisites} </p>
+                                </div>
+                            </div>
                         </div>
 
                         {/* Details */}
-                        <div className='hidden w-full lg:inline-flex flex-col justify-start items-start space-y-1 '>
+                        {/* <div className='hidden w-full lg:inline-flex flex-col justify-start items-start space-y-1 '>
 
                             <p className='text-black text-base font-Roboto font-medium'>
                                 Goal : {' '}
@@ -69,7 +93,7 @@ const CommunityCourseCard = ({ communityCourseData }: IProps) => {
                                 </span>
                             </p>
 
-                        </div>
+                        </div> */}
                     </div>
 
                 </div>

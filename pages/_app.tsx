efd935b1c270b645fa80f3b0bd1ce632b39510a0
Anurag <anuragmarketing101@gmail.com>
2023-01-18 18:47:27 +0530

    communitySettingsButton-added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import BaseOneLayout from '../components/layouts/BaseOne/BaseOneLayout'
 import { Provider } from 'react-redux'
 import store from "../redux/store"
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 // import { Bebas_Neue } from "@next/font/google"
 // import { Roboto } from "@next/font/google"
 
@@ -12,9 +15,21 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Provider store={store}>
-        <BaseOneLayout >
-          <Component {...pageProps} />
-        </BaseOneLayout>
+      <BaseOneLayout >
+        <Component {...pageProps} />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </BaseOneLayout>
     </Provider>
   )
 }
